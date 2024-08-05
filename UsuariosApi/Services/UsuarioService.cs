bda74c2b45db2340b22e7c8ef5669dd7a001f77a
Thiago Lima <thiagofslima@gmail.com>
2024-08-05 14:25:47 -0300

    Criando Login

--- UsuariosApi/Services/UsuarioService.cs
+++ UsuariosApi/Services/UsuarioService.cs
@@ -5,15 +5,17 @@ using UsuariosApi.Models;
 
 namespace UsuariosApi.Services;
 
-public class CadastroService
+public class UsuarioService
 {
     private IMapper _mapper;
     private UserManager<Usuario> _userManager;
+    private SignInManager<Usuario> _signInManager;
 
-    public CadastroService(IMapper mapper, UserManager<Usuario> userManager)
+    public UsuarioService(IMapper mapper, UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
     {
         _mapper = mapper;
         _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     public async Task Cadastra(CreateUsuarioDto dto)
@@ -24,5 +26,13 @@ public class CadastroService
 
         if (!resultado.Succeeded)
             throw new ApplicationException("Falha ao cadastrar usuário!");
+    }
+
+    public async Task Login(LoginUsuarioDto dto)
+    {
+        var resultado = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
+
+        if (!resultado.Succeeded)
+            throw new ApplicationException("Usuário não autenticado!");
     }
 }
